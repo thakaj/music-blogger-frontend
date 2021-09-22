28e86cee0178aba26f4e88d19fdeda1acb3be60c
thakaj <taulant.hakaj@scranton.edu>
2021-09-22 18:49:37 -0400

    I changed an error in my code that i forgot to fix.

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
         <NavBar user={user} handleLogout={handleLogout}/>
         <Switch> 
           <Route exact path= "/login" >
-            <Login handleLogin={handleLogin} setUser={setUser}  />
+            <Login handleLogin={handleLogin} user={user} />
           </Route>
           <Route exact path ="/">
             <SignupPage user={user} />
